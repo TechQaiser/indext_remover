27b1b35fb9e23ca49b82d97119c32ab442e00d76
Qaiser Abbas <69212320+TechQaiser@users.noreply.github.com>
2022-10-20 23:06:21 +0500

    Create indext_remover.py

---
+++ indext_remover.py
@@ -0,0 +1,47 @@
+import os,time
+
+#getting input of file location
+
+file = input("file path : ")
+
+try:
+
+	benzene = open(file,'r').read()except FileNotFoundError:
+
+	exit(" Not Found File ")
+
+except:
+
+	exit("Something Wrong ! ")
+
+#reading the content of input file
+
+#if file have indext then process will start
+
+if "    " in benzene:
+
+	print("Collecting All Indext Spaces ....")
+
+	time.sleep(2.7)
+
+	#---(replacing-spaces)----#
+
+	os.system(f"sed -i 's/    /	/g' {file}")
+
+	os.system(f"sed -i 's/        /		/g' {file}")
+
+	os.system(f"sed -i 's/            /			/g' {file}")
+
+	os.system(f"sed -i 's/                /				/g' {file}")
+
+	os.system(f"sed -i 's/                    /					/g' {file}")
+
+	print("Completed Removing Indext ðŸ”¥ ")
+
+	time.sleep(3)
+
+	exit()
+
+else:
+
+	exit("This File Have No Indext Spaces To Remove .")
